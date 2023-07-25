09fe9c3a255602832acbdc847cd9ef871f3510da
Paras Arya <parasarya289@gmail.com>
2023-07-26 01:38:39 +0530

    feat: scroll and bug fixes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,19 +1,21 @@
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { englishToHindiMap } from "../../LangaugeMap/LangaugeMap";
 import "./Home.css";
 
 export const Home = () => {
   const [userInput, setUserInput] = useState("");
   const [toBeTyped, setToBeTyped] = useState(
-    "quick brown fox lying in the box,हिन्दी जिसके मानकीकृत रूप को मानक हिन्दी कहा जाता है"
+    "The quick brown fox jumps over the lazy dog"
   );
   const [backSpacePressed, setBackSpacePressed] = useState(0);
+  const [langauge, setLanguage] = useState("universal");
 
-  const [langauge, setLanguage] = useState("hindi");
-
+  const containerRef = useRef(null);
   const toBeTypedRef = useRef(null);
   const userInputRef = useRef(null);
   const userPastedText = useRef(null);
+
+  const MAX_INPUT_LENGTH = toBeTyped?.replaceAll("\u200D", "")?.length;
 
   const processInput = (e) => {
     const { value } = e.target;
@@ -33,9 +35,10 @@ export const Home = () => {
         return (acc += englishToHindiMap[char] ?? "");
       }, "");
       setUserInput(convertedText.replaceAll("\u200D", ""));
-      console.log(convertedText)
+      console.log(convertedText);
       return;
     }
+    // if (userInputRef?.current?.value?.length)
 
     setUserInput(value);
   };
@@ -45,59 +48,70 @@ export const Home = () => {
     const userInputWord = userInput.replaceAll("\u200D", "").split(" ");
 
     let count = 0;
+    let wordCount = 0;
     let corrects = [];
+    if (userInputWord.length <= toBeTypedWord.length) {
+      if (userInput?.length && toBeTyped.length) {
+        userInputWord?.forEach((userWord, idx) => {
+          let tempWordCount = 0;
+          const { count: eachCharCount, elements } = [...userWord]?.reduce(
+            (acc, char, i) => {
+              console.log(char);
+              let toBeTypedChar = toBeTypedWord[idx][i];
+              // let convertedChar =
+              //   langauge === "hindi"
+              //     ? englishToHindiMap[toBeTypedChar]
+              //     : toBeTypedChar;
+              console.log({ char, toBeTypedChar });
+              if (char.replace("\u200D", "") === toBeTypedChar) {
+                return {
+                  count: acc.count + 1,
+                  elements: [
+                    ...acc.elements,
+                    {
+                      correct: true,
+                      index: i,
+                      word: char,
+                    },
+                  ],
+                };
+              } else {
+                return {
+                  count: acc.count + 1,
+                  elements: [
+                    ...acc.elements,
+                    {
+                      correct: false,
+                      index: i,
+                      word: char,
+                    },
+                  ],
+                };
+              }
+            },
+            { count: 0, elements: [] }
+          );
+          count += eachCharCount;
+          corrects = [...corrects, elements];
 
-    if (userInput.length && toBeTyped.length) {
-      userInputWord?.forEach((userWord, idx) => {
-        const { count: eachWordCount, elements } = [...userWord]?.reduce(
-          (acc, char, i) => {
-            console.log(char);
-            let toBeTypedChar = toBeTypedWord[idx][i];
-            let convertedChar =
-              langauge === "hindi"
-                ? englishToHindiMap[toBeTypedChar]
-                : toBeTypedChar;
-            console.log({ char, toBeTypedChar });
-            if (char.replace("\u200D", "") === toBeTypedChar) {
-              return {
-                count: acc.count + 1,
-                elements: [
-                  ...acc.elements,
-                  {
-                    correct: true,
-                    index: i,
-                    word: char,
-                  },
-                ],
-              };
-            } else {
-              return {
-                count: acc.count,
-                elements: [
-                  ...acc.elements,
-                  {
-                    correct: false,
-                    index: i,
-                    word: char,
-                  },
-                ],
-              };
-            }
-          },
-          { count: 0, elements: [] }
-        );
-        count += eachWordCount;
-        corrects = [...corrects, elements];
-      });
+          tempWordCount += eachCharCount;
+          if (tempWordCount === toBeTypedWord[idx]?.length) {
+            wordCount += 1;
+          }
+        });
+      }
     }
-    return { count, corrects };
+
+    return { count, wordCount, corrects };
   };
+
+  const { count, wordCount, corrects } = useMemo(() => {
+    return checkCorrectCount();
+  }, [userInput, toBeTyped]);
 
   const renderText = () => {
     const toBeTypedWord = toBeTyped.replaceAll("\u200D", "").split(" ");
     const userInputWord = userInput.replaceAll("\u200D", "").split(" ");
-
-    const { corrects } = checkCorrectCount();
 
     const allText = [];
 
@@ -105,9 +119,9 @@ export const Home = () => {
       const correctWord = corrects[wordIndex];
       const userTypedWord = userInputWord[wordIndex] || "";
 
-      let heighlight = "";
+      let highlight = "";
       if (userInputWord.length - 1 === wordIndex && userInput.length >= 1) {
-        heighlight = { backgroundColor: "yellow" };
+        highlight = "highlight";
       }
 
       for (let charIndex = 0; charIndex < word.length; charIndex++) {
@@ -120,20 +134,14 @@ export const Home = () => {
 
         if (correct === true) {
           allText.push(
-            <span style={{ color: "green", ...heighlight }}>
-              {currentWord}
-            </span>
+            <span className={`correct ${highlight}`}>{currentWord}</span>
           );
         } else if (correct === false) {
           allText.push(
-            <span style={{ color: "red", ...heighlight }}>
-              {currentWord}
-            </span>
+            <span className={`incorrect ${highlight}`}>{currentWord}</span>
           );
         } else {
-          allText.push(
-            <span style={{ ...heighlight }}>{word[charIndex]}</span>
-          );
+          allText.push(<span className={highlight}>{word[charIndex]}</span>);
         }
       }
       if (userTypedWord.length > word.length) {
@@ -145,7 +153,10 @@ export const Home = () => {
         ) {
           const userTypedChar = userTypedWord[charIndex];
           allText.push(
-            <span style={{ color: "red", ...heighlight }}>
+            <span
+              className={`incorrect ${highlight}`}
+              // key={`${userTypedChar}-${charIndex}`}
+            >
               {userTypedChar}
             </span>
           );
@@ -155,6 +166,11 @@ export const Home = () => {
     });
     return allText;
   };
+
+  const memoizedRenderText = useMemo(
+    () => renderText(),
+    [userInput, toBeTyped]
+  );
 
   const changeText = (e) => {
     e.preventDefault();
@@ -168,45 +184,58 @@ export const Home = () => {
     }
   };
 
-  const moveCarret = (e) => {
-    const { value } = e.target.value;
-    setUserInput(value);
-
-    if (toBeTypedRef.current) {
-      toBeTypedRef.current.selectionStart = 4;
-      toBeTypedRef.current.selectionEnd = 4;
-    }
-  };
-
   const handleBackSpace = (e) => {
     if (e.key === "Backspace" && userInput) {
       setBackSpacePressed((prev) => prev + 1);
     }
   };
 
+  useEffect(() => {
+    const highlightedElement = document.querySelector(
+      ".textToBeTyped > .highlight"
+    );
+    console.log(highlightedElement);
+    if (highlightedElement) {
+      highlightedElement.scrollIntoView({
+        behavior: "auto",
+        block: "start",
+      });
+    }
+  }, [userInput]);
+
   return (
     <main className="home">
-      <h2>{userInput}</h2>
+      <h6>{userInput}</h6>
       <h4>
-        {checkCorrectCount()?.count} / {toBeTyped.replaceAll(" ", "").length}{" "}
-        Backspace: {backSpacePressed}{" "}
-        <select onChange={(e) => setLanguage(e.target.value)}>
-          <option value="hindi">Hindi</option>
-          <option value="english">English</option>
+        {count} / {toBeTyped.replaceAll(" ", "").length} {wordCount} /
+        {toBeTyped.split(" ").length} Backspace: {backSpacePressed}{" "}
+        <select onChange={(e) => setLanguage(e.target.value)} value={langauge}>
+          <option value="hindi" disabled>
+            Hindi
+          </option>
+          <option value="universal">Universal</option>
         </select>
       </h4>
       <section className="typing-section">
-        {/* <textarea ref={toBeTypedRef} value={toBeTyped} /> */}
-        <div className="textToBeTyped">{renderText()}</div>
+        <div ref={containerRef} className="textToBeTyped">
+          {memoizedRenderText}
+        </div>
+
         <form onSubmit={changeText}>
           <textarea ref={userPastedText} placeholder="Paste your text here" />
           <button type="submit">Submit</button>
         </form>
         <textarea
+          ref={userInputRef}
           placeholder="start typing here"
           onChange={(e) => processInput(e)}
           onKeyDown={(e) => handleBackSpace(e)}
-          // value={userInput}
+          value={userInput}
+          // maxLength={MAX_INPUT_LENGTH}
+          onMouseDown={(e) => {
+            userInputRef?.current?.focus();
+            e.preventDefault();
+          }}
         />
       </section>
     </main>
